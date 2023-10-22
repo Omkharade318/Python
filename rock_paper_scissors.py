8e4d4c66814cd4a03729e46ad49534a93ec238fd
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2023-10-22 20:43:34 +0530

    Add files via upload
    A Python program for playing stone, paper, scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,31 @@
+import random
+
+def check(comp, user):
+    if comp == user:
+      return 0
+  
+    if (comp == 'Stone' and user == 'scissor'):
+        return -1
+    
+    if (comp==2 and user==3):
+        return -1
+    
+    if (comp==3 and user==1):
+        return -1
+    
+    return 1
+
+comp = random.randint(1,3)
+user = int (input("Press 1 for Stone, 2  for Scissors and 3 for Paper:  "))
+
+score = check(comp, user)
+
+print("You choose:",str(user))
+print("Computer choose:",str(comp))
+
+if (score == 0):
+    print("Its a draw")
+elif (score == -1):
+    print("You Lose") 
+else:
+    print("You won")       
