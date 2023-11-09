671dcc5d785e8383d016122638f95f81a3eafcd6
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2023-11-09 23:21:12 +0530

    Add files via upload
    Shows how actually the break and continue statement works

---
+++ break_continue.py
@@ -0,0 +1,11 @@
+for i in [2,3,4,5,6,7,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
+
+print("\n")
+    
+for i in range(1,15):
+    if (i==11):
+        break
+    print("5 x",i,"=",5*i)
