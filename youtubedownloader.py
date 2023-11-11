66ae7dc4a50566843ba08b7c437d98a9e1b6b69c
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2023-11-11 19:34:21 +0530

    Update youtubedownloader.py
    Just follow the instructions to download your video

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,5 +1,10 @@
 import pytube
-link = "https://youtu.be/zEngh-u4CX0?feature=shared" 
-yt = pytube.YouTube(link)
+link = input("Paste the link here: ")               #provide your YouTube link here
+yt = pytube.YouTube(link)         
 stream = yt.streams.get_highest_resolution()
-stream.download()
+stream.download('provide a path')       
+''' in the () provide the location of the folder 
+in which you want to download or else 
+it'll download in the same folder in which your code exists. 
+It'll take some time to download, 
+depending upon the length and max resolution of the video''' 
